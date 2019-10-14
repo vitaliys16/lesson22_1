4aca35093b0c411c1262084e6e3997416e5de128
vitaliys16 <55391973+vitaliys16@users.noreply.github.com>
2019-10-14 23:50:31 +0300

    правки
    Устранение правок

--- lesson22_1/js/validator.js
+++ lesson22_1/js/validator.js
@@ -33,5 +33,70 @@ $(document).ready(function(){
         },
     });
     /*Маска для телефона*/
-    $('.phone').mask ('8 (999) 999-99-99')
+    $('.phone').mask ('8 (999) 999-99-99');
+
+    $('#offer-form').validate({
+        errorElement: "div",
+        errorClass: "invalid-offer",
+        rules: {
+            username: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+        },
+            email:{
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true,
+            },
+        },
+        messages: {
+            username: {
+                required: "Укажите имя",
+                minlength: jQuery.validator.format("Осталось символов: {0}"),
+                maxlength: jQuery.validator.format("Максимальная длинна имени 15 символов")
+            },
+            email: {
+                email: "Введите корректный email",
+                required: "Укажите email"
+            }, 
+            phone: {
+                required: "Укажите номер телефона"
+            },  
+        },
+    });
+    $('#modal-form').validate({
+        errorElement: "div",
+        errorClass: "invalid",
+        rules: {
+            username: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+        },
+            email:{
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true,
+            },
+        },
+        messages: {
+            username: {
+                required: "Укажите имя",
+                minlength: jQuery.validator.format("Осталось символов: {0}"),
+                maxlength: jQuery.validator.format("Максимальная длинна имени 15 символов")
+            },
+            email: {
+                email: "Введите корректный email",
+                required: "Укажите email"
+            }, 
+            phone: {
+                required: "Укажите номер телефона"
+            },  
+        },
+    });
 })
